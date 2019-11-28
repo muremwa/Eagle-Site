1f08d8ec27cc9183cbd288f5314a210ad36e8e15
Muremwa <danmburu254@gmail.com>
2019-11-28 18:07:52 +0300

    added commenting function to the blog

--- eagle/blog/views.py
+++ eagle/blog/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404, redirect, reverse
 
 from .models import Post
 
@@ -16,3 +17,17 @@ class PostPage(generic.DetailView):
     context_object_name = 'post'
     template_name = 'blog/post_single.html'
     slug_url_kwarg = 'slug'
+
+    def post(self, *args, **kwargs):
+        post = get_object_or_404(Post, slug=kwargs['slug'])
+        comment_email = self.request.POST['email']
+        if not comment_email:
+            comment_email = None
+
+        comment = post.comment_set.create(
+            name=self.request.POST['name'],
+            email=comment_email,
+            message=self.request.POST['message'],
+        )
+
+        return redirect(comment.get_absolute_url())
