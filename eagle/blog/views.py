08658ef98ec849ee3f3278ca52870d8fc98a56e5
Muremwa <danmburu254@gmail.com>
2022-06-01 23:13:54 +0300

    separate post filtering

--- eagle/blog/views.py
+++ eagle/blog/views.py
@@ -7,42 +7,45 @@ from .models import Post
 from datetime import date
 
 
-# blog home
-class AllPostsPage(generic.ListView):
-    template_name = 'blog/posts.html'
-    context_object_name = 'posts'
-    model = Post
+class PostsFilter:
 
     def get_queryset(self):
         q_set = Post.objects.filter(published__exact=True)
 
         # filter by author's name
         if 'author' in self.request.GET:
-            q_set = q_set.filter(author__name__icontains=self.request.GET['author'])
+            q_set = q_set.filter(author__name__icontains=self.request.GET.get('author', ''))
 
         # filter by tags
         if 'tag' in self.request.GET:
-            q_set = q_set.filter(tags__name__icontains=self.request.GET['tag'])
+            q_set = q_set.filter(tags__name__icontains=self.request.GET.get('tag', ''))
 
         # posts before said date
         if 'before' in self.request.GET:
-            date_ = [int(i) for i in self.request.GET['before'].split("-")]
+            date_ = [int(i) for i in self.request.GET.get('before', '').split("-")]
             date_ = date(date_[0], date_[1], date_[2])
             q_set = q_set.filter(created__lt=date_)
 
         # posts on the said date
         if 'on' in self.request.GET:
-            date_ = [int(i) for i in self.request.GET['on'].split("-")]
+            date_ = [int(i) for i in self.request.GET.get('on', '').split("-")]
             date_ = date(date_[0], date_[1], date_[2])
             q_set = q_set.filter(created__iexact=date_)
 
         # posts after said date
         if 'after' in self.request.GET:
-            date_ = [int(i) for i in self.request.GET['after'].split("-")]
+            date_ = [int(i) for i in self.request.GET.get('after', '').split("-")]
             date_ = date(date_[0], date_[1], date_[2])
             q_set = q_set.filter(created__gt=date_)
 
         return q_set
+
+
+# blog home
+class AllPostsPage(PostsFilter, generic.ListView):
+    template_name = 'blog/posts.html'
+    context_object_name = 'posts'
+    model = Post
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
